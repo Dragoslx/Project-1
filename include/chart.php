57bff4982fd06133f43b307693e0efe0e91160be
SunJzhut <39034492+SunJzhut@users.noreply.github.com>
2018-05-20 19:42:05 -0400

    Delete chart.php

--- include/chart.php
+++
@@ -1,8 +0,0 @@
-<script src="//www.amcharts.com/lib/3/amcharts.js"></script>
-<script src="//www.amcharts.com/lib/3/serial.js"></script>
-<script src="//www.amcharts.com/lib/3/themes/light.js"></script>
-<script src="//www.amcharts.com/lib/3/amstock.js"></script>
-
-<div id="chartdiv" style="width: 100%; height: 700px;"></div>
-
-<script src="/js/chart.js"></script>
