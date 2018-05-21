b9a5913a5b6a29492b88fd3c6d98b079d67290e7
Dragoslx <39471164+Dragoslx@users.noreply.github.com>
2018-05-20 20:46:06 -0400

    Update head-home.php

--- include/head-home.php
+++ include/head-home.php
@@ -8,7 +8,7 @@
     <meta name="description" content="<?php __('meta.description') ?>">
     <meta name="author" content="">
     <link rel="shortcut icon" href="favicon.ico">
-    <title><?php __('meta.title'); ?></title>
+    <title>SpermPonzi</title>
 
     <!-- Font Awesome -->
     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
@@ -42,104 +42,6 @@
       })(window, document, 'script', 'dataLayer', 'GTM-P2FJV5S')</script>
     <!-- End Google Tag Manager -->
 </head>
-
-<body class="lang_<?php echo getLangCode(); ?> home">
-<!-- Google Tag Manager (noscript) -->
-<noscript>
-    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P2FJV5S"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>
-</noscript>
-<!-- End Google Tag Manager (noscript) -->
-
-<!-- Sidebar Menu -->
-<div class="ui vertical inverted sidebar menu">
-    <a class="item" href="index.html"><?php __('menu.home'); ?></a>
-    <a class="item" href="coins.html"><?php __('menu.coins'); ?></a>
-    <a class="item" href="https://ethpyramid.wordpress.com"><?php __('menu.blog'); ?></a>
-    <a class="item" href="stats.html"><?php __('menu.stats'); ?></a>
-    <a class="item" href="whitepaper.html"><?php __('menu.whitepaper'); ?></a>
-    <a class="item" href="https://etherscan.io/address/0x2fa0ac498d01632f959d3c18e38f4390b005e200"><?php __('index.header.contract'); ?></a>
-</div>
-
-<!-- Following Menu -->
-<div class="ui large top fixed hidden menu inverted">
-    <div class="ui container">
-        <a class="toc item">
-            <i class="sidebar icon"></i>
-        </a>
-        <a href="index.html" class="logo header item">
-            <img src="images/minilogo.png">
-        </a>
-        <a class="item" href="coins.html"><?php __('menu.coins'); ?></a>
-        <a class="item" href="https://ethpyramid.wordpress.com"><?php __('menu.blog'); ?></a>
-        <a class="item" href="stats.html"><?php __('menu.stats'); ?></a>
-        <a class="item" href="whitepaper.html"><?php __('menu.whitepaper'); ?></a>
-        <div class="right menu">
-            <div class="social-bits">
-                <a href="https://twitter.com/ethpyramid" target="_blank">
-                    <i class="fab fa-twitter-square"
-                       style="margin-right:10px; color:white;"></i></a>
-                <a href="https://www.reddit.com/r/ethpyramid/" target="_blank">
-                    <i class="fab fa-reddit-square"
-                       style="margin-right:10px; color:white;"></i></a>
-                <a href="https://discord.gg/FmKsvBe" target="_blank">
-                    <i class="fab fa-discord"
-                       style="color:white"></i></a>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Page Contents -->
-<div class="pusher">
-    <div class="ui masthead vertical center aligned segment">
-        <div class="ui container main-menu">
-            <div class="ui large secondary top inverted menu">
-                <a class="toc item">
-                    <i class="sidebar icon"></i>
-                </a>
-                <a class="item" href="coins.html"><?php __('menu.coins'); ?></a>
-                <a class="item" href="https://ethpyramid.wordpress.com"><?php __('menu.blog'); ?></a>
-                <a class="item" href="stats.html"><?php __('menu.stats'); ?></a>
-                <a class="item" href="whitepaper.html"><?php __('menu.whitepaper'); ?></a>
-
-                <div class="right item">
-                    <!-- Social Icons -->
-                    <div class="social-bits">
-                        <a href="https://twitter.com/ethpyramid" target="_blank">
-                            <i class="fab fa-twitter-square"
-                               style="margin-right:10px; color:white;"></i></a>
-                        <a href="https://www.reddit.com/r/ethpyramid/" target="_blank">
-                            <i class="fab fa-reddit-square"
-                               style="margin-right:10px; color:white;"></i></a>
-                        <a href="https://discord.gg/FmKsvBe" target="_blank">
-                            <i class="fab fa-discord"
-                               style="color:white"></i></a>
-                    </div>
-
-                    <!-- Language Selector -->
-                    <div class="ui inline dropdown lang-selector">
-                        <a href="#" class="text">
-                            <i class="<?php echo getLangCode(); ?> flag"></i>
-                        </a>
-                        <i class="dropdown icon"></i>
-                        <div class="menu">
-                            <a href="?lang=us" class="item text"><i class="us flag"></i> English (US)</a>
-                            <a href="?lang=au" class="item text"><i class="au flag"></i> English (AU)</a>
-                            <a href="?lang=sa" class="item text"><i class="sa flag"></i> Arabic</a>
-                            <a href="?lang=cn" class="item text"><i class="cn flag"></i> Chinese</a>
-                            <a href="?lang=fr" class="item text"><i class="fr flag"></i> French</a>
-                            <a href="?lang=de" class="item text"><i class="de flag"></i> German</a>
-                            <a href="?lang=in" class="item text"><i class="in flag"></i> Hindi</a>
-                            <a href="?lang=jp" class="item text"><i class="jp flag"></i> Japanese</a>
-                            <a href="?lang=kr" class="item text"><i class="kr flag"></i> Korean</a>
-                            <a href="?lang=es" class="item text"><i class="es flag"></i> Spanish</a>
-                            <a href="?lang=ru" class="item text"><i class="ru flag"></i> Russian</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
 
         <!-- Jumbotron -->
         <div class="ui text container jumbotron">
