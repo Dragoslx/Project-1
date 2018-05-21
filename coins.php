2b39f6ac3dd116449d04344e067280ced21be67e
Dragoslx <39471164+Dragoslx@users.noreply.github.com>
2018-05-20 20:33:13 -0400

    Update coins.php

--- coins.php
+++ coins.php
@@ -3,27 +3,4 @@ include('include/lang.php');
 include('include/head.php');
 ?>
 
-<p align="center"><a href="http://www.pyrmex.com">PyrMex Futures Market Now Live!</a></p>
-
 <?php include('include/token_sale.php'); ?>
-
-<div class="ui container">
-    <h3 class="ui horizontal divider"><?php __('chart.heading'); ?></h3>
-	<?php include('include/chart.php'); ?>
-</div>
-
-<div class="ui container vertical stripe">
-    <h3 class="ui horizontal divider"><?php __('instructions.heading'); ?></h3>
-    <ul>
-        <li><?php __('instructions.metamask'); ?></li>
-        <li><?php __('instructions.purchase'); ?></li>
-        <li><?php __('instructions.your-epy'); ?></li>
-        <li><?php __('instructions.get-divs'); ?></li>
-        <li><?php __('instructions.sell-epy'); ?></li>
-        <li><?php __('instructions.pay-divs'); ?></li>
-        <li><?php __('instructions.withdraw'); ?></li>
-        <li><?php __('instructions.getmeout'); ?></li>
-    </ul>
-</div>
-
-<?php include('include/foot.php'); ?>
